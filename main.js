46638d2f23dc35e5bb643b407ac5ba9276ef2ad6
Domavem <61091132+Domavem@users.noreply.github.com>
2020-10-17 21:00:24 +0900

    JS: Navbar menu click
    Scroll to clicked menu on navbar

--- main.js
+++ main.js
@@ -12,3 +12,18 @@ document.addEventListener('scroll', ()=>{
         navbar.classList.remove('navbar--dark')
     }
 });
+
+//Scroll to clicked menu on navbar
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event)=> {
+    
+    const target = event.target;
+    const link = target.dataset.link;
+    if(link ==null){
+        return;
+    }
+    
+    console.log(event.target.dataset.link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: 'smooth'});
+});
